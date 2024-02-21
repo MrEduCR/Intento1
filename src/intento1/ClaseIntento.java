6029bf242a9e755dddf0c04457a4e1d3f9181422
MrEduCR <145067441+MrEduCR@users.noreply.github.com>
2024-02-20 20:24:23 -0600

    Primer commit

---
+++ src/intento1/ClaseIntento.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package intento1;
+
+/**
+ *
+ * @author eduga
+ */
+public class ClaseIntento {
+    int hola = 1;
+    
+    
+}
---
+++ src/intento1/Intento1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package intento1;
+
+/**
+ *
+ * @author eduga
+ */
+public class Intento1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int numero = 2;
+        System.out.println("Hola");
+        
+    }
+    
+}
